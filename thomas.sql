1222435ce757cd672237fed14d7274e42e65e6bc
ThomasBonderup <tb@tbcoding.dk>
2020-10-08 17:56:27 +0200

    String search for D.4 and D.10

--- thomas.sql
+++ thomas.sql
@@ -1,12 +1,10 @@
 -- D.2 Simple Search
 
--- a string_search FUNCTION
 CREATE OR REPLACE FUNCTION string_search (string char(50), uconst char(10))
--- ASsumes a table title - the main part of the source table title_bASics
-RETURNS table (
+
+RETURNS TABLE (
   tconst char(10),
-  primarytitle text,
-  plot text
+  primarytitle text
 ) 
 LANGUAGE plpgsql
 AS $$
@@ -23,6 +21,60 @@ SELECT * FROM string_search('Remake', 'ui000123');
 SELECT * FROM string_search('Frozen', 'ui000123')
 SELECT * FROM string_search('Frozen 2', 'ui000123');
 
+-- D.4 Structured String Search
+
+CREATE OR REPLACE FUNCTION structured_string_search(title char(50), plot char(50), characters char(50), name char(50), uconst char(10))
+
+RETURNS TABLE (
+  tconst char(10),
+  primarytitle text
+) 
+LANGUAGE plpgsql
+AS $$
+BEGIN
+INSERT INTO search_history(uconst, timestamp, search)
+VALUES(uconst, NOW(), CONCAT(title, plot, characters, name)); 
+RETURN query SELECT t.tconst, t.primarytitle 
+FROM title NATURAL JOIN title_principals NATURAL JOIN name t
+WHERE (t.title IS NULL OR t.title = title)
+AND (t.plot IS NULL OR t.plot = plot)
+END
+$$;
+
+SELECT * FROM structured_string_search('test', 'test', 'test', 'test', 'ui000123');
+
+SELECT * FROM structured_string_search('', 'see', '', 'Mads miKKelsen', 'ui000123');
+
+
+
+-- D.10 Structured String Search
+CREATE OR REPLACE FUNCTION structured_string_search(title char(50), plot char(50), characters char(50), name char(50), uconst char(10))
+
+RETURNS TABLE (
+  tconst char(10),
+  primarytitle text
+) 
+LANGUAGE plpgsql
+AS $$
+BEGIN
+INSERT INTO search_history(uconst, timestamp, search)
+VALUES(uconst, NOW(), CONCAT(title, plot, characters, name));
+RETURN query SELECT t.tconst, t.primarytitle 
+FROM title t,
+(SELECT wi.tconst from wi where word = title
+UNION
+SELECT wi.tconst from wi where word = plot
+UNION
+SELECT wi.tconst from wi where word = characters
+UNION
+SELECT wi.tconst from wi where word = name) w
+WHERE t.tconst = w.tconst;
+END
+$$;
+
+SELECT * FROM structured_string_search('test', 'test', 'test', 'test', 'ui000123');
+
+SELECT * FROM structured_string_search('', 'see', '', 'Mads miKKelsen', 'ui000123');
 
 -- Trigger function to insert search query in search_history table
 CREATE OR REPLACE FUNCTION insert_search_history() RETURNS event_trigger AS $$
