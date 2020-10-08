4839916d6ac5d44c05a7589f193ab207bafff9ca
emilieunna <ebul@ruc.dk>
2020-10-08 11:36:43 +0200

    inserted users

--- C2_build_framework_db.sql
+++ C2_build_framework_db.sql
@@ -94,9 +94,9 @@ ALTER TABLE movie_data_model.name_bookmark OWNER TO postgres;
 
 -- Inserts users into user table
 insert into movie_data_model.user
-values ('ui000123', 'Alex', 'Tao', 'alextao@hotmail.com', '********', 'alextao01');
-values ('ui000124', 'Nils', 'Mullenborn', 'nimu@hotmail.com', '********', 'nimu01');
-values ('ui000125', 'Thomas', 'Winther', 'thwi@hotmail.com', '********', 'thwi01');
-values ('ui000126', 'Emilie', 'Unna', 'emun@hotmail.com', '********', 'emun01');
-values ('ui000127', 'Troels', 'Andreasen', 'tran@hotmail.com', '********', 'tran01');
-values ('ui000128', 'Henrik', 'Bulskov', 'hebu@hotmail.com', '********', 'hebu01');
+values ('ui000123', 'Alex', 'Tao', 'alextao@hotmail.com', '********', 'alextao01'),
+('ui000124', 'Nils', 'Mullenborn', 'nimu@hotmail.com', '********', 'nimu01'),
+('ui000125', 'Thomas', 'Winther', 'thwi@hotmail.com', '********', 'thwi01'),
+('ui000126', 'Emilie', 'Unna', 'emun@hotmail.com', '********', 'emun01'),
+('ui000127', 'Troels', 'Andreasen', 'tran@hotmail.com', '********', 'tran01'),
+('ui000128', 'Henrik', 'Bulskov', 'hebu@hotmail.com', '********', 'hebu01');
