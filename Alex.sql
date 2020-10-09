ed42ef59da3c8932cc22f07fc49356916bfd7135
Alex Tao <alexwoko@gmail.com>
2020-10-09 21:04:37 +0200

    Update Alex.sql
    Implementet triggers for rating history entry + new average rating

--- Alex.sql
+++ Alex.sql
@@ -1,17 +1,5 @@
-CREATE OR REPLACE FUNCTION rate(user_id CHARACTER(10), movie_id CHARACTER(10), movie_rating INTEGER, movie_review TEXT)
-RETURNS void LANGUAGE plpgsql AS $$
-BEGIN
 
-INSERT INTO 
-rating (uconst, tconst, rating, review) 
-
-VALUES (user_id, 
-movie_id,
-movie_rating, 
-movie_review);
-
-END $$;
-
+/*---------USER RATING - BASIC ---------*/
 
 CREATE OR REPLACE FUNCTION rate(user_id CHARACTER(10), movie_id CHARACTER(10), movie_rating INTEGER)
 RETURNS void LANGUAGE plpgsql AS $$
@@ -27,19 +15,35 @@ movie_rating,
 
 END $$;
 
-CREATE OR REPLACE FUNCTION update_rating_history()
-RETURNS TRIGGER LANGUAGE plpgsql AS
 
-$$ 
-DECLARE 
-arg TEXT;
+/*---------USER RATING - OVERLOADED ---------*/
+
+CREATE OR REPLACE FUNCTION rate(user_id CHARACTER(10), movie_id CHARACTER(10), movie_rating INTEGER, movie_review TEXT)
+RETURNS void LANGUAGE plpgsql AS $$
+BEGIN
+
+INSERT INTO 
+rating (uconst, tconst, rating, review) 
+
+VALUES (user_id, 
+movie_id,
+movie_rating, 
+movie_review);
+
+END $$;
+
+/*---------UPDATE RATING HISTORY - TRIGGER ---------*/
+
+CREATE OR REPLACE FUNCTION update_rating_history()
+RETURNS TRIGGER LANGUAGE plpgsql AS $$ 
 
 BEGIN
 
-RETURN NEW;
+INSERT INTO rating_history(uconst, tconst, timestamp, rating, review)
+VALUES (NEW.uconst, NEW.tconst, NOW(), NEW.rating, NEW.review);
 
-END;
-$$
+RETURN NEW;
+END; $$
 
 
 CREATE TRIGGER insert_rating_history 
@@ -47,4 +51,22 @@ AFTER INSERT OR UPDATE ON rating
 FOR EACH ROW 
 EXECUTE PROCEDURE update_rating_history();
 
+/*---------UPDATE AVRG RATING - TRIGGER ---------*/
 
+CREATE OR REPLACE FUNCTION update_avrg_rating()
+RETURNS TRIGGER LANGUAGE plpgsql AS $$ 
+
+BEGIN
+
+UPDATE title_ratings 
+SET numvotes = numvotes + 1,
+averagerating = (averagerating) + ((NEW.rating - averagerating) / numvotes)
+WHERE tconst = NEW.tconst;
+
+RETURN NEW;
+END; $$
+
+CREATE TRIGGER upd_avrg_rating_trigger
+AFTER INSERT OR UPDATE ON rating 
+FOR EACH ROW 
+EXECUTE PROCEDURE update_avrg_rating();
