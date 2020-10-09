2c23d3934ae1fdda23eb4e654a098b1ccec16ce3
emilieunna <ebul@ruc.dk>
2020-10-09 18:31:21 +0200

    Update unna.sql

--- unna.sql
+++ unna.sql
@@ -1,1 +1,13 @@
-﻿
+-- Find coplayers
+create or replace view co_actor_data as
+select title_principals.tconst, name.nconst, name.primaryname
+from name, title_principals;
+
+select nconst, primaryname
+from co_actor_data
+where tconst in (select tconst
+from co_actor_data
+where primaryname like 'Lauren Bacall'
+group by tconst
+order by count(primaryname))
+limit 12;﻿
