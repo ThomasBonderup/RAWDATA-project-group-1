d60db800eecd536f80b1248b9cac9e91e24ab5a9
ThomasBonderup <tb@tbcoding.dk>
2020-10-10 08:53:32 +0200

    Fixed D.2 simple search and D.4 structured search + D.5 and D.11

--- thomas.sql
+++ thomas.sql
@@ -1,62 +1,132 @@
--- D.2 Simple Search
+-- D.2 Simple Search function
+-- searching on primary title or plot for movie
+-- search string is stored in search history as a side effect when function is called
+-- returns table with tconst and primary title
+DROP FUNCTION IF EXISTS string_search (search_string CHARACTER(50), uconst CHARACTER(10));
 
-CREATE OR REPLACE FUNCTION string_search (string char(50), uconst char(10))
+CREATE OR REPLACE FUNCTION string_search (search_string CHARACTER(50), uconst CHARACTER(10))
 
 RETURNS TABLE (
-  tconst char(10),
+  tconst CHARACTER(10),
   primarytitle text
-) 
+)
+
 LANGUAGE plpgsql
 AS $$
+
 BEGIN
+
+-- stores search string in search history with timestamp
 INSERT INTO search_history(uconst, timestamp, search)
-VALUES(uconst, NOW(), string); 
+VALUES(uconst, NOW(), search_string); 
+
+-- returns table by using ILIKE for string pattern matching
+-- ILIKE is used to pattern match and lower and upper case characters
+-- % matches sequences of zero or more characters
+-- || is used for string concatenation
 RETURN query SELECT t.tconst, t.primarytitle 
 FROM title t
-WHERE t.primarytitle LIKE '%'||string||'%' OR t.plot LIKE '%'||string||'%';
+WHERE t.primarytitle ILIKE '%'||search_string||'%' OR t.plot ILIKE '%'||search_string||'%';
+
 END
 $$;
 
+-- D.2 Simple Search function test
 SELECT * FROM string_search('Remake', 'ui000123');
-SELECT * FROM string_search('Frozen', 'ui000123')
-SELECT * FROM string_search('Frozen 2', 'ui000123');
 
--- D.4 Structured String Search
+-- D.4 Structured String Search function
+-- searching with 4 parameters title, plot, characters, name and uconst for more precise search queries on title
+-- search string is stored in search history as a side effect when function is called
+-- returns table with tconst and primary title
+DROP FUNCTION IF EXISTS structured_string_search(title CHARACTER(50), plot CHARACTER(50), characters CHARACTER(50),name CHARACTER(50), uconst CHARACTER(10));
 
-CREATE OR REPLACE FUNCTION structured_string_search(title char(50), plot char(50), characters char(50), name char(50), uconst char(10))
+CREATE OR REPLACE FUNCTION structured_string_search(title CHARACTER(50),
+						    plot CHARACTER(50),
+						    characters CHARACTER(50),
+       	  	  	   			    name CHARACTER(50),
+						    uconst CHARACTER(10))
 
 RETURNS TABLE (
-  tconst char(10),
+  tconst CHARACTER(10),
   primarytitle text
-) 
+)
+
 LANGUAGE plpgsql
 AS $$
+
 BEGIN
+
+-- stores search string in search history with timestamp
 INSERT INTO search_history(uconst, timestamp, search)
 VALUES(uconst, NOW(), CONCAT(title, plot, characters, name)); 
-RETURN query SELECT t.tconst, t.primarytitle 
-FROM title NATURAL JOIN title_principals NATURAL JOIN name t
-WHERE (t.title IS NULL OR t.title = title)
-AND (t.plot IS NULL OR t.plot = plot)
+
+
+-- returns table by using ILIKE for string pattern matching
+-- ILIKE is used to pattern match and lower and upper case characters
+-- % matches sequences of zero or more characters
+-- || is used for string concatenation
+RETURN query SELECT title.tconst, title.primarytitle
+FROM title NATURAL JOIN title_principals NATURAL JOIN name
+WHERE title.primarytitle 	       ILIKE '%'||structured_string_search.title||'%'
+AND   title.plot		       ILIKE '%'||structured_string_search.plot||'%'
+AND   title_principals.characters      ILIKE '%'||structured_string_search.characters||'%'
+AND   name.primaryname 		       ILIKE '%'||structured_string_search.name||'%';
 END
 $$;
 
-SELECT * FROM structured_string_search('test', 'test', 'test', 'test', 'ui000123');
-
+-- D.4 Structured String Search function test
+-- test case with lower and upper case characters including empty parameters
 SELECT * FROM structured_string_search('', 'see', '', 'Mads miKKelsen', 'ui000123');
 
+-- D.5 actor_search function
+-- searching on primary name of actor or character
+-- search string is stored in search history as a side effect when function is called
+-- returns table with nconst and primary name
+DROP FUNCTION IF EXISTS actor_search(string CHARACTER(50), uconst CHARACTER(10));
 
-
--- D.10 Structured String Search
-CREATE OR REPLACE FUNCTION structured_string_search(title char(50), plot char(50), characters char(50), name char(50), uconst char(10))
+CREATE OR REPLACE FUNCTION actor_search(string CHARACTER(50), uconst CHARACTER(10))
 
 RETURNS TABLE (
-  tconst char(10),
-  primarytitle text
-) 
+  nconst CHARACTER(10),
+  primaryname VARCHAR(256)
+)
+
 LANGUAGE plpgsql
 AS $$
+
 BEGIN
+
+-- stores search string in search history with timestamp
+INSERT INTO search_history(uconst, timestamp, search)
+VALUES(uconst, NOW(), string);
+
+-- returns table by using ILIKE for string pattern matching
+-- ILIKE is used to pattern match and lower and upper case characters
+-- % matches sequences of zero or more characters
+-- || is used for string concatenation
+RETURN query SELECT name.nconst, name.primaryname 
+FROM name NATURAL JOIN title_principals
+WHERE name.primaryname ILIKE '%'||string||'%' OR title_principals.characters ILIKE '%'||string||'%';
+
+END
+$$;
+
+-- D.5 actor_search function test
+SELECT * FROM actor_search('Mads Mikkelsen', 'ui000123');
+
+-- D.11 Exact-match Querying
+CREATE OR REPLACE FUNCTION exact_match_search(title CHARACTER(50), plot CHARACTER(50), characters CHARACTER(50), name CHARACTER(50), uconst CHARACTER(10))
+
+RETURNS TABLE (
+  tconst CHARACTER(10),
+  primarytitle text
+) 
+
+LANGUAGE plpgsql
+AS $$
+
+BEGIN
+
 INSERT INTO search_history(uconst, timestamp, search)
 VALUES(uconst, NOW(), CONCAT(title, plot, characters, name));
 RETURN query SELECT t.tconst, t.primarytitle 
@@ -69,21 +139,8 @@ SELECT wi.tconst from wi where word = characters
 UNION
 SELECT wi.tconst from wi where word = name) w
 WHERE t.tconst = w.tconst;
+
 END
 $$;
 
-SELECT * FROM structured_string_search('test', 'test', 'test', 'test', 'ui000123');
-
-SELECT * FROM structured_string_search('', 'see', '', 'Mads miKKelsen', 'ui000123');
-
--- Trigger function to insert search query in search_history table
-CREATE OR REPLACE FUNCTION insert_search_history() RETURNS event_trigger AS $$
-BEGIN
-	INSERT INTO search_history(uconst, timestamp, search)
-	VALUES('1', NOW(), 'search string');
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE EVENT TRIGGER search_history_trigger ON string_search EXECUTE PROCEDURE insert_search_history();
-
-
+SELECT * FROM exact_match_search('', 'see', '', 'Mads miKKelsen', 'ui000123');
