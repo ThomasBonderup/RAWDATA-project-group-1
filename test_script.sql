b0e9e30244aadae4789c930b5f1ed7f79ea2997f
ThomasBonderup <tb@tbcoding.dk>
2020-10-11 08:50:07 +0200

    changed uconst id in test scritp

--- test_script.sql
+++ test_script.sql
@@ -1,20 +1,21 @@
 -- GROUP: RAW1, MEMBERS: <Alex Tao Korsgaard Wogelius>, <Emilie Beske Unna-Lindhard>, <Nils Müllenborn>, <Thomas Winther Bonderup>
 
+SET search_path TO movie_data_model;
 -- D.1.
 
 -- D.2 Simple Search
 -- Test case: 
-SELECT * FROM string_search('Remake', 'ui000123');
+SELECT * FROM string_search('Remake', 'ui000001');
 
 -- D.3
 
 -- D.4 Structured String Search
 -- Test case: lower and upper case characters including empty parameters
-SELECT * FROM structured_string_search('', 'see', '', 'Mads miKKelsen', 'ui000123');
+SELECT * FROM structured_string_search('', 'see', '', 'Mads miKKelsen', 'ui000001');
 
 -- D.5 Actor Search
 -- Test case: 
-SELECT * FROM actor_search('Mads Mikkelsen', 'ui000123');
+SELECT * FROM actor_search('Mads Mikkelsen', 'ui000001');
 
 -- D.6
 
@@ -28,11 +29,11 @@ SELECT * FROM actor_search('Mads Mikkelsen', 'ui000123');
 
 -- D.11 Exact-match Querying
 -- Test case: 
-SELECT * FROM exact_match_search('apple', 'mads', 'mikkelsen', 'ui000123');
+SELECT * FROM exact_match_search('apple', 'mads', 'mikkelsen', 'ui000001');
 
 -- D.12 Best-match Querying
 -- Test case:
-SELECT * FROM exact_match_search('apple', 'mads', 'mikkelsen', 'ui000123');
+SELECT * FROM exact_match_search('apple', 'mads', 'mikkelsen', 'ui000001');
 
 -- Test case: dynamic function using VARIADIC array for multiple input parameters
 SELECT * FROM dynamic_bestmatch('apple', 'mads', 'mikkelsen');
